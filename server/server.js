6b59be44701c2548bd56cc8e0f84f781c22c4df1
Kai Sawamoto <kaisawamoto@gmail.com>
2020-03-07 14:35:34 -0800

    first commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-
+const mysql = require('mysql');
+const dotenv = require('dotenv');
 app.use(express.json());
 
-const port = 4000;
+dotenv.config();
+
+const con = mysql.createConnection({
+	host: process.env.HOST,
+	user: process.env.USERNAME,
+	password: process.env.PASSWORD,
+	database: process.env.DB_NAME
+});
+
+const PORT = 4000;
 app.listen(port, () => {
-	console.log(`Server started on http://localhost:${port}`);
+	console.log(`Server started on http://localhost:${PORT}`);
 });
 
 app.get('/', (req, res) => {
